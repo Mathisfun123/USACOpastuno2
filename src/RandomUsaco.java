32f84d3f109d0fa8d9a233db6e0b665518ea6a90
Mathisfun123 <sairajareddykurelli@gmail.com>
2018-05-31 17:12:44 -0500

    Working on Badmilk -2015: Bronze  December

--- src/RandomUsaco.java
+++ src/RandomUsaco.java
@@ -2,9 +2,8 @@ import java.util.HashMap;
 
 public class RandomUsaco {
     public static void main(String[] args) {
-        HashMap <Integer, Integer> items= new HashMap<>();
-        items.put(5,5);
-        items.put(6,5);
-        System.out.println(items.put(2,7));
+        double hi = 5.1223;
+        int b = 100;
+        System.out.println(hi+b);
     }
 }
