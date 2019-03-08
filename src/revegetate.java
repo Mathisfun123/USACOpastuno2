6ca40574ced9a60b0e4f83789bc263aec99c9a94
Mathisfun123 <sairajareddykurelli@gmail.com>
2019-03-07 21:32:28 -0600

    Finish Revegetate- Remember to cast new Integer(val) for ArrayList

--- src/revegetate.java
+++ src/revegetate.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.TreeSet;
 
@@ -14,73 +15,35 @@ public class revegetate {
 		for(int i = 0; i< n; i++){
 			pastures[i]= new Pasture(i+1);
 		}
+		int ans[]  = new int[n];
 		for(int i = 0; i< cows; i++){
 			int s1 = sc.nextInt(); int s2 = sc.nextInt();
 			pastures[s1-1].addConnection(s2); pastures[s2-1].addConnection(s1);
 		}
-
-
-		pastures[0].seednum=1; pastures[0].hasSeed=true;
-		for(int i = 1; i< n; i++){
-			if(!pastures[i].hasSeed){
-				if(!pastures[0].items.contains(i+1)){
-					pastures[i].seednum=1; pastures[i].hasSeed=true;
-					pastures[0].items.addAll(pastures[i].items);
+		for(int i = 0; i < n; i++){
+			ArrayList<Integer> poss = new ArrayList<>();
+			poss.add(1); poss.add(2); poss.add(3);poss.add(4);
+			//System.out.println(pastures[i].items);
+			for(int j = 0; j< pastures[i].items.size(); j++){
+				if(ans[pastures[i].items.get(j)-1]!=0){
+					poss.remove( new Integer(ans[pastures[i].items.get(j)-1]));
 				}
 			}
+			ans[i] = poss.get(0);
+			//printTime(ans);
 		}
-		int spot2 = -1;
-		for(int i = 1; i< n; i++) {
-			if (!pastures[i].hasSeed){
-				if(spot2==-1){
-					spot2=i; pastures[i].hasSeed=true; pastures[i].seednum=2;
-				}else{
-					if(!pastures[i].items.contains(spot2+1)){
-						pastures[i].hasSeed=true; pastures[i].seednum=2;
-						pastures[spot2].items.addAll(pastures[i].items);
-
-					}
-				}
-			}
+		StringBuilder outln = new StringBuilder();
+		for (int i = 0; i < ans.length; i++) {
+			int an = ans[i];
+			outln.append(an);
 		}
-		int spot3 = -1;
-		for(int i = 1; i< n; i++){
-			if(!pastures[i].hasSeed){
-				if(spot3==-1){
-					spot3=i; pastures[i].hasSeed=true; pastures[i].seednum=3;
-				}else{
-					if(!pastures[i].items.contains(spot3+1)){
-						pastures[i].hasSeed=true; pastures[i].seednum=3;
-						pastures[spot3].items.addAll(pastures[i].items);
-
-					}
-				}
-			}
-		}
-		int spot4 = -1;
-		for(int i = 1; i< n; i++){
-			if(!pastures[i].hasSeed){
-				if(spot4==-1){
-					spot4=i; pastures[i].hasSeed=true; pastures[i].seednum=4;
-				}else{
-					if(!pastures[i].items.contains(spot4+1)){
-						pastures[i].hasSeed=true; pastures[i].seednum=4;
-						pastures[spot4].items.addAll(pastures[i].items);
-
-					}
-				}
-			}
-		}
-		for (int i = 0; i < pastures.length; i++) {
-			Pasture pasture = pastures[i];
-			out.print(pasture.seednum);
-		}
+		out.println(outln.toString());
 		out.close();
 	}
 	public static class Pasture{
 		public int id, seednum;
 		boolean hasSeed= false;
-		public TreeSet<Integer> items = new TreeSet<>();
+		public ArrayList<Integer> items = new ArrayList<>();
 		public Pasture(int i){
 			id=i;
 		}
@@ -92,5 +55,12 @@ public class revegetate {
 		public String toString() {
 			return id+ " " + items + seednum;
 		}
+	}
+	public static void printTime(int [] arr){
+		for (int i = 0; i < arr.length; i++) {
+			int i1 = arr[i];
+			System.out.print(i1);
+		}
+		System.out.println();
 	}
 }
